3728a60c4965c6d93aa02f7f7b3e5a8beaa3889e
Matt Zukowski <matt@zukowski.ca>
2011-10-24 23:29:36 -0400

    Authentication is now handled by Rollcall. User accounts in sail.social are automatically created when a new Rollcall user logs in.

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,7 +1,17 @@
 package controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import models.Profile;
 import models.User;
+import play.Logger;
+import play.Play;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
+import play.libs.WS.WSRequest;
 
 /**
  * Checks user login pass
@@ -11,8 +21,40 @@ import models.User;
  */
 public class Security extends controllers.Secure.Security {
 
+	
 	static boolean authenticate(String username, String password) {
-		return User.connect(username, password) != null;
+		// authenticate with rollcall
+		String rollcallUrl = Play.configuration.getProperty("sail.rollcall.url");
+		
+		WSRequest req = WS.url(rollcallUrl+"/login.json");
+		req.setParameter("session[login]", username);
+		req.setParameter("session[password]", password);
+		WS.HttpResponse res = req.post();
+		
+		JsonElement json = res.getJson();
+		
+		if (res.getStatus() == 201) { // session created successfully
+			JsonObject session = json.getAsJsonObject().getAsJsonObject("session");
+			JsonObject account = session.getAsJsonObject("account");
+			
+			String token = session.get("token").getAsString();
+			String encryptedPassword = account.get("encrypted_password").getAsString();
+			
+			// now check if we already have this user in the local database
+			if (User.findUserByUsername(username) == null) { // user doesn't yet exist
+				(new User(username, null)).save();
+				Logger.info("User '"+username+"' created and authenticated.");
+				return true;
+			} else { // user exists
+				Logger.info("User '"+username+"' successfuly authenticated.");
+				return true;
+			}
+			
+		} else { // some error during session creation
+			Logger.warn("Authentication for '"+username+"' failed because: "+json.toString());
+			flash.put("error", "Rollcall authentication failed: "+json.toString());
+			return false;
+		}
 	}
 	
 	static void onAuthenticated() {
