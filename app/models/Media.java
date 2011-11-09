4db8b100224611174d3bbadc043581d16b8d7ced
Anthony Perritano <aperritano@gmail.com>
2011-11-09 11:57:10 +0100

    added media stuff

--- app/models/Media.java
+++ app/models/Media.java
@@ -1,5 +1,8 @@
 package models;
 
+
+import java.util.List;
+
 import javax.persistence.Entity;
 
 import play.db.jpa.Model;
@@ -8,14 +11,24 @@ import play.db.jpa.Model;
 public class Media extends Model {
 	
 	public String url;
-	public String type;
+	public String mediaTyp;
 	public String origin;
 
 	
 	public Media(String url, String type, String origin) {
 		this.url = url;
-		this.type = type;
+		this.mediaTyp = type;
 		this.origin = origin;
+	}
+	
+	public static List<Media> findMediaByImage(){
+		String mediaTyp = "IMAGE";
+		return find("byMediaTyp", mediaTyp).fetch();
+	}
+	
+	public static List<Media> findMediaByVideo(){
+		String mediaTyp = "video";
+		return find("byMediaType", mediaTyp).fetch();
 	}
 
 }
